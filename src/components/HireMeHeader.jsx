d141694445b74bd5667c397f990c747d3e579063
raj nagoriya <rajnagoriya3@gmail.com>
2026-02-27 11:51:56 +0530

    hero section header improve and download resume option added

--- src/components/HireMeHeader.jsx
+++ src/components/HireMeHeader.jsx
@@ -1,22 +1,25 @@
-import 'remixicon/fonts/remixicon.css';
+import "remixicon/fonts/remixicon.css";
+import { RiFileDownloadLine } from "react-icons/ri";
+const RESUME_PATH = "/file/raj_resume_mca.pdf";
 
 const HireMeHeader = () => {
-  
-  // Function to scroll to the bottom
-  const scrollToBottom = () => {
-    window.scrollTo({
-      top: document.body.scrollHeight, // Scroll to the bottom
-      behavior: "smooth", // Smooth scrolling effect
-    });
+  const handleDownloadResume = () => {
+    const link = document.createElement("a");
+    link.href = RESUME_PATH;
+    link.download = "";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
-    <div className="fixed w-full z-10 p-20 flex items-center justify-end">
-      <button 
-        className="bg-palette-light hover:opacity-90 px-4 py-2 border-2 border-palette-light font-[anzo3] text-palette-dark transition-all"
-        onClick={scrollToBottom}
+    <div className="fixed w-full z-10 p-10 md:p-20 flex items-center justify-end">
+      <button
+        className="bg-palette-light flex items-center justify-center px-4 py-2 font-[anzo3] text-palette-dark hover:text-palette-medium transition-all border-2 border-palette-dark"
+        onClick={handleDownloadResume}
       >
-        Hire Me
+        <RiFileDownloadLine className="text-2xl mr-2" />
+        Download Resume
       </button>
     </div>
   );
